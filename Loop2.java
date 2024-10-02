e68ad9981564a23f22f3676eb1934870135d8e96
RAGINI <ragininishad333@gmail.com>
2024-10-02 14:25:44 +0530

    Add files via upload

---
+++ Loop2.java
@@ -0,0 +1,17 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        // System.out.println(1);
+       // System.out.println(2);
+       // System.out.println(3);
+       //System.out.println("Using loops");
+        int i = 1;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        
+        }
+/*while(true){
+    System.out.println("I am an infinte while loop!");
+}*/
+    }
+}
