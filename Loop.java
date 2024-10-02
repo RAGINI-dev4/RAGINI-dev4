80310fa8ab267e0ac076a5a54d2c5de9592db60f
RAGINI <ragininishad333@gmail.com>
2024-10-02 14:26:17 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,12 @@
+public class Loop{
+    public static void main(String[] args) {
+        int age =36;
+        boolean cond=(age==18);
+        if(cond){
+            System.out.println("YES BOY U CAN DRIVE");
+        }
+       else{
+           System.out.println("no boy u cannnot drive");
+       }
+        }
+    }
