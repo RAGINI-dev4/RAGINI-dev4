bcac68d57cadfbf72bf61242b0ff56215b919bd5
RAGINI <ragininishad333@gmail.com>
2024-10-03 18:57:58 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,17 @@
+
+public class Character {
+    public static void main(String[] args) {
+       //  char a = 'A';
+//System.out.println("print:"+a);
+ //String p = "Ragini";
+  //ystem.out.println(" Name of the girl is:"+p);
+  //System.out.print("The national bird of india is peacock:");
+ // boolean a = true;
+ // System.out.println(a);
+ //float m = 5.6f;
+ //float n = 4.8f;
+ //float sum = m+n;
+ //System.out.println(sum);
+ System.out.println("THANKING YOU FOR TODAYS LESSON");
+            }
+}
