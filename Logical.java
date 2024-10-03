3ca842b03b79626ba88eab5978d501dff192aa46
RAGINI <ragininishad333@gmail.com>
2024-10-03 18:54:36 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,29 @@
+public class Logical {
+    public static void main(String[] args) {
+        //System.out.println("LOGICAL AND");
+        boolean a =false;
+        boolean b = true;
+      //  if(a&&b){
+    //        System.out.println("yes");
+  //      }
+//else{
+  //  System.out.println("no");
+//}
+  //  }
+//}
+//System.out.println("LOGICAL OR");
+//if(a||b){
+  //  System.out.println("yes");
+//}
+//else{
+  //  System.out.println("no");
+//}
+    //}
+//}
+System.out.println("LOGICAL NOT");
+System.out.print("Not(a) is ");
+System.out.println(!a);
+System.out.print("Not(b) is ");
+System.out.println(!b);
+    }
+}
