b57729f7235ad81288751365c216bdec235f72b5
RAGINI <ragininishad333@gmail.com>
2024-10-03 18:59:38 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // DOFFERENT WAYS OF ARRAYS
+        // 1. DECLARATION AND MEMOMERY ALLOCATION
+       // int [] marks = new int[5];
+//2. DECLARATION AND THEN MEMORY ALLOCATION
+//int [] marks;
+ //marks= new int[3];
+ //INTIALIZATON
+     /*   marks[0]=100;
+        marks[1]=89;
+        marks[2]=89;
+        System.out.println(marks[2]);
+    }*/
+    int[] marks ={98,77,75,44,66}; // 3.DECLAR + INTIALIATION
+        System.out.println(marks[4]);
+    }
+}// 3*4=12 BYTES
