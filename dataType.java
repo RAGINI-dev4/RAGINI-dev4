2d98dcc8e46d820704a45a2b85518b302ee5ac1d
RAGINI <ragininishad333@gmail.com>
2024-10-10 17:30:44 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,26 @@
+public class dataType {
+    public static void main(String[] args) {
+        System.out.println("types of data tpes");
+        String name ="there are are two types of data type";
+        System.out.println(name);
+        String a="primitive data type";
+        String b="non primitive data type";
+System.out.println(a);
+System.out.println(b);
+//primitve data types are:- int , str ,char,byte,float,long,short,boolean
+int c= 24;
+int d= 98;
+int f=c+d;
+System.out.println(f);
+char i ='A';
+System.out.println( "we use single quotation when we use char data type");
+System.out.println(i);
+System.out.println("we use double quotation data type when we use str data type");
+float j=4.5f;
+float k=9.8f;
+float sum=j+k;
+System.out.println(sum);
+System.out.println("we use f at last of decimal digit when we use float data type");
+
+}
+}
