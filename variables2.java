fe403398d53d0d5408d89e95836535acb82a7b18
RAGINI <ragininishad333@gmail.com>
2024-10-10 17:32:54 +0530

    Add files via upload

---
+++ variables2.java
@@ -0,0 +1,16 @@
+
+
+
+public class variables2 {
+    public static void main(String[] args) {
+        int a= 98;
+        int b=98;
+      int c =a+b;
+        System.out.println("the  sum of a and b is:"+c);
+       
+    
+    
+    }
+}
+
+
