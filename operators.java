38d637a7fca364a9860ac2ede81192299ce83fb5
RAGINI <ragininishad333@gmail.com>
2024-10-10 17:31:46 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,15 @@
+public class operators {
+    public static void main(String[] args) {
+        int a= 345;
+        int b=45;
+        int sum=a+b;
+        System.out.println("the sum of a and b is:"+sum);
+        float c= 6.7f;
+        float d=7.8f;
+        float e=c*d;
+        System.out.println("the product of c and d is:"+e);
+       int mean=a+b/2;
+       System.out.println("the mean of a and b is:"+mean);
+        
+    }
+}
