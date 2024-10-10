b4c38af6ec554ae50658b100037b940b06cac96b
RAGINI <ragininishad333@gmail.com>
2024-10-10 17:31:10 +0530

    Add files via upload

---
+++ JavaIf.java
@@ -0,0 +1,12 @@
+public class JavaIf {
+    public static void main(String[] args) {
+        System.out.println("IF STATEMENT");
+    String name = "ragini";
+    int roll = 57;
+    if(name =="ragini" && roll==57){
+     System.out.println("details of ragini");
+    }else {
+      System.out.println("INVALID DETAILS");
+    }
+    }
+}
