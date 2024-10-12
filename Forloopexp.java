fee76055d15c3a0553532b147d125aa7777cf5e4
RAGINI <ragininishad333@gmail.com>
2024-10-13 02:05:27 +0530

    Add files via upload

---
+++ Forloopexp.java
@@ -0,0 +1,16 @@
+public class Forloopexp {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+System.out.println(2*i+1);
+}
+System.out.println("even numbers");
+int n;
+for(n=1;n<=15;n++){
+    System.out.println(2*n);
+
+}
+        }
+    }
+
+//odd numbers and even numbers
