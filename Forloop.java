9c7b8b0108c801299bd8ef8ca100e2dffac0bc7e
RAGINI <ragininishad333@gmail.com>
2024-10-13 02:05:00 +0530

    Add files via upload

--- Forloop.java
+++ Forloop.java
@@ -1,20 +1,16 @@
 public class Forloop {
-     public static void main(String[] args) {
-     /*   for(int i=1; i<=10; i++){
+    public static void main(String[] args) {
+int i;
+        for(i=1;i<=10;i++){
             System.out.println(i);
+            int n;
+            for(n=1;n<=10;n++){
+                System.out.println(2*n+1);
+                
+                }
         }
+
     }
-}*/
-// 2n=  even number
-// 2n+1 =odd number
-//int n =9;
-//System.out.println(n);
-//
-//for (int i=0; i<5;i++){
-  //  System.out.println(2*i+1);
-//}
-for (int i=0; i<9; i++){
-    System.out.println(2*i);
 }
-     }
-    }
+    
+
