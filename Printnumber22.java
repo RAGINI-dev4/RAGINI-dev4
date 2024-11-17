361eed351b8a40ea4456ab530ac645ae57ccd650
RAGINI <ragininishad333@gmail.com>
2024-11-18 00:26:37 +0530

    Add files via upload
    print number 1 to n

---
+++ Printnumber22.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Printnumber22 {
+    public static void main(String[] args) {  System.out.println("enter range");
+        Scanner sc = new Scanner(System.in);
+        int range=sc.nextInt();
+    
+        int i=1;
+        while(i<=range){
+            System.out.println(i+" ");
+            i++;
+
+        }
+        }
+}
