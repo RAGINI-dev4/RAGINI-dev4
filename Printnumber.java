19791d61ec614a4569910829053a10b071992564
RAGINI <ragininishad333@gmail.com>
2024-11-17 23:09:26 +0530

    Add files via upload
    PRINT NUMBER 1 T 10

---
+++ Printnumber.java
@@ -0,0 +1,14 @@
+// print number 1 to 10
+public class Printnumber {
+    public static void main(String[] args) {
+        int counter =1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
+ // BY USING WHILE LOOP
+ // WHILE(CONDITION){
+ // SOUT();
+ //}
